f958821af62c7db1b87fca32d70fcf02f76ad240
Ceyhun Qurbanzadə <c.gurbanzade@asan.gov.az>
2022-08-29 17:52:43 +0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
